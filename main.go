0ee3da673196985f9ea8e13d6c80bbf751bf0102
Vladimir Dulenov <originalrad50@gmail.com>
2023-11-13 22:14:21 +0100

    feat: mvp of Manufacturer funcs was created

--- main.go
+++ main.go
@@ -1,16 +1,43 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"fmt"
 	"log"
 
 	"github.com/netbox-community/go-netbox/netbox"
+	"github.com/netbox-community/go-netbox/netbox/client"
+	"github.com/netbox-community/go-netbox/netbox/client/dcim"
+	"github.com/netbox-community/go-netbox/netbox/models"
 )
 
 var (
 	publicNetboxURI = "https://demo.netbox.dev/"
 )
+
+func createDcimManufacturer(nbClient *client.NetBoxAPI, vendorDevice models.Manufacturer) error {
+	_, err := nbClient.Dcim.DcimManufacturersCreate(&dcim.DcimManufacturersCreateParams{
+		Context: context.TODO(),
+		Data:    &vendorDevice,
+	}, nil)
+	if err != nil {
+		return fmt.Errorf("DCIM Manufactur creation failed. device: %s. error: %w", vendorDevice.Display, err)
+	}
+	return nil
+}
+
+func isManufacturerExist(nbClient *client.NetBoxAPI, slug string) (found bool, id int64, err error) {
+	_, err = nbClient.Dcim.DcimManufacturersList(&dcim.DcimManufacturersListParams{
+		Context: context.TODO(),
+		SlugIe:  &slug,
+	}, nil)
+	if err != nil {
+		return found, id, fmt.Errorf("DCIM Manufactur finding failed. slug: %v. error: %w", &slug, err)
+	}
+	// TODO:
+	return false, id, nil
+}
 
 func main() {
 	apiToken := flag.String("token", "", "netbox api token")
